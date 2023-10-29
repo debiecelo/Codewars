1fedda61ce894ed2746c6a3bd833ee43e39afed4
debiecelo <michal.d9777@gmail.com>
2023-10-29 14:13:46 +0100

    highest-and-lowest-solution

---
+++ src/HighestAndLowest.java
@@ -0,0 +1,26 @@
+package src;
+
+import java.util.Arrays;
+
+import static java.util.Arrays.stream;
+
+public class HighestAndLowest {
+    public static void main(String[] args) {
+        System.out.println(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+    }
+
+    private static String highAndLow(String numbers) {
+        String[] split = numbers.split(" ");
+        int size = split.length;
+            int [] array = new int [size];
+            for (int i = 0; i < size; i++){
+                array[i] = Integer.parseInt(split[i]);
+            }
+
+        int[] ints = Arrays.stream(array).sorted().toArray();
+        String maximum = String.valueOf(ints[size-1]);
+        String s1 = String.valueOf(ints[0]);
+        return maximum + " " + s1;
+
+    }
+}
